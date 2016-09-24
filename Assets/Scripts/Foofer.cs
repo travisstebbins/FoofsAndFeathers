4316030e69d162eb8950034891d8b993f4ada9c3
Travis Stebbins <stebbins.travis@gmail.com>
2016-09-24 10:06:44 -0500

    Implement Foofer generator and add Foofer collisions with walls

---
+++ Assets/Scripts/Foofer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foofer : MonoBehaviour {
+
+	// public variables
+	public float maxSpeed = 10f;
+	public float randomVariance = 2f;
+
+	// components
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = new Vector2 (Random.Range (-maxSpeed, maxSpeed), Random.Range (-maxSpeed, maxSpeed));
+		//rb.AddForce(new Vector2(Random.Range(-randomVariance, randomVariance), Random.Range(-randomVariance, randomVariance)));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rb.AddForce(new Vector2(Random.Range(-randomVariance, randomVariance), Random.Range(-randomVariance, randomVariance)));
+		//rb.velocity = new Vector2 ((Mathf.Abs(rb.velocity.x) > maxSpeed) ?
+		//	rb.velocity.x + Random.Range (-randomVariance, randomVariance) : rb.velocity.x,
+		//	(Mathf.Abs(rb.velocity.y) > maxSpeed ?
+		//		rb.velocity.y + Random.Range (-randomVariance, randomVariance) : rb.velocity.y));
+		rb.velocity = new Vector2(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed));
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag("Boundary")) {
+			rb.velocity = new Vector2(-rb.velocity.x, -rb.velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/FooferGenerator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FooferGenerator : MonoBehaviour {
+
+	// public variables
+	public GameObject fooferPrefab;
+	public int minWaitTime = 10;
+	public int maxWaitTime = 25;
+
+	// private variables
+	private int waitTime;
+	private float currentTime;
+	private float lastSpawnTime;
+	private Transform leftBound;
+	private Transform lowerBound;
+
+	// Use this for initialization
+	void Start () {
+		waitTime = Random.Range (minWaitTime, maxWaitTime + 1);
+		currentTime = Time.time;
+		lastSpawnTime = Time.time;
+		leftBound = GameObject.FindGameObjectWithTag ("LeftBound").GetComponent<Transform> ();
+		lowerBound = GameObject.FindGameObjectWithTag ("LowerBound").GetComponent<Transform> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		currentTime = Time.time;
+		if (currentTime - lastSpawnTime >= waitTime) {
+			//Transform trans = new GameObject ().transform;
+			//trans.position = new Vector3 (Random.Range (leftBound.position.x, leftBound.position.x * -1), Random.Range (lowerBound.position.y, lowerBound.position.y * -1), 0);
+			//trans.position = new Vector3 (0, 0, 0);
+			GameObject.Instantiate (fooferPrefab, new Vector3 (Random.Range (leftBound.position.x, leftBound.position.x * -1), Random.Range (lowerBound.position.y, lowerBound.position.y * -1), 0), Quaternion.identity);
+			//Instantiate (fooferPrefab, new Vector3 (Random.Range (leftBound.position.x, leftBound.position.x * -1), Random.Range (lowerBound.position.y, lowerBound.position.y * -1), 0), Quaternion.identity);
+			lastSpawnTime = Time.time;
+			waitTime = Random.Range (minWaitTime, maxWaitTime + 1);
+		}
+	}
+}
