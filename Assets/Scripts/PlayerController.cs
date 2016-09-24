5222d3de0bd5d83ba9e4a3327d5d74a993e93e2b
Travis Stebbins <stebbins.travis@gmail.com>
2016-09-24 04:46:02 -0500

    Begin implementing foofed feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,21 @@ public class PlayerController : MonoBehaviour {
 
 	// public variables
 	public float maxSpeed = 3f;
+	public bool foofed = true;
+	public float foofedParticleEmissionRate = 20f;
 
 	// components
 	private Rigidbody2D rb;
+	private CircleCollider2D cc;
 	private GameManager gm;
+	private ParticleSystem particles;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		cc = GetComponent<CircleCollider2D> ();
 		gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+		particles = GetComponent<ParticleSystem> ();
 	}
 	
 	// Update is called once per frame
@@ -22,6 +28,14 @@ public class PlayerController : MonoBehaviour {
 			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal1") * maxSpeed, Input.GetAxis ("Vertical1") * maxSpeed);
 		} else if (CompareTag("Player2")) {
 			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal2") * maxSpeed, Input.GetAxis ("Vertical2") * maxSpeed);
+		}
+		if (foofed) {
+			cc.enabled = true;
+			particles.startLifetime = 5;
+			var emission = particles.emission;
+			var rate = emission.rate;
+			rate.constantMax = foofedParticleEmissionRate;
+			emission.rate = rate;
 		}
 	}
 
