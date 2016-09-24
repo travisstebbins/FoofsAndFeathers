5ce49ecbc4d3ea18a8bd14cc002ac1a7edef1a2d
Travis Stebbins <stebbins.travis@gmail.com>
2016-09-24 03:01:07 -0500

    Create scripts to allow characters to pass over objects and change between saturated/desaturated

---
+++ Assets/Scripts/ColorableObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorableObject : MonoBehaviour {
+
+	public float transitionSpeed = 0.05f;
+
+	// private variables
+	private bool inColor = true;
+
+	// components
+	SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inColor && rend.color.a < 1) {
+			Debug.Log ("transitioning from BW to color");
+			rend.color = new Color (rend.color.r, rend.color.g, rend.color.b, rend.color.a + transitionSpeed);
+			if (rend.color.a > 1)
+				rend.color = new Color (rend.color.r, rend.color.g, rend.color.b, 1);
+		} else if (!inColor && rend.color.a > 0) {
+			Debug.Log ("transitioning from color to BW");
+			rend.color = new Color (rend.color.r, rend.color.g, rend.color.b, rend.color.a - transitionSpeed);
+			if (rend.color.a < 0)
+				rend.color = new Color (rend.color.r, rend.color.g, rend.color.b, 0);
+		}
+	}
+
+	public void setColor () {
+		inColor = true;
+	}
+
+	public void setBW () {
+		inColor = false;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	// public variables
+	public float maxSpeed = 3f;
+
 	// components
 	private Rigidbody2D rb;
 
@@ -14,14 +17,27 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (CompareTag("Player1")) {
-			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal1"), Input.GetAxis ("Vertical1"));
-		}
-		else if (CompareTag("Player2")) {
-			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal2"), Input.GetAxis ("Vertical2"));
+			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal1") * maxSpeed, Input.GetAxis ("Vertical1") * maxSpeed);
+		} else if (CompareTag("Player2")) {
+			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal2") * maxSpeed, Input.GetAxis ("Vertical2") * maxSpeed);
 		}
 	}
 
 	void FixedUpdate () {
 
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.CompareTag ("ColorableObject")) {
+			Debug.Log ("ColorableObject triggered");
+			ColorableObject co = (ColorableObject)other.gameObject.GetComponent<ColorableObject> ();
+			if (CompareTag ("Player1")) {
+				Debug.Log ("co set color");
+				co.setColor ();
+			} else if (CompareTag ("Player2")) {
+				Debug.Log ("co set BW");
+				co.setBW ();
+			}
+		}
 	}
 }
