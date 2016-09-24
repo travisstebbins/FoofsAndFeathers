40159dd2ccf4a6c8939e3f7393fb0517b9e6bad9
Travis Stebbins <stebbins.travis@gmail.com>
2016-09-23 23:14:03 -0500

    Create PlayerController script and add basic movement functionality

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// components
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CompareTag("Player1")) {
+			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal1"), Input.GetAxis ("Vertical1"));
+		}
+		else if (CompareTag("Player2")) {
+			rb.velocity = new Vector2 (Input.GetAxis ("Horizontal2"), Input.GetAxis ("Vertical2"));
+		}
+	}
+
+	void FixedUpdate () {
+
+	}
+}
