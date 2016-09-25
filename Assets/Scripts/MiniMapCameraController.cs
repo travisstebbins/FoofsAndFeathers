b2b23c0c63f9481243d298efebcf3ed171522922
Travis Stebbins <stebbins.travis@gmail.com>
2016-09-25 13:16:50 -0500

    Implement foofer sprite

---
+++ Assets/Scripts/MiniMapCameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMapCameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera c = GetComponent<Camera> ();
+		c.aspect = 2.2388f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
